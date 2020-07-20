99d074f9b8ba6cbb69adb151e86100ff87348b21
Sumeet Singh mankoo <sumeet.mankoo@gmail.com>
2020-07-20 14:22:48 -0400

    this time with correct brew path

--- script-library/homebrew.sh
+++ script-library/homebrew.sh
@@ -7,11 +7,11 @@ sudo yum -y install libxcrypt-compat || true
 
 # Install homebrew
 export CI=1
-/bin/bash -c "$(echo -e "\r\n" | curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-eval $(~/.linuxbrew/bin/brew shellenv)
+/bin/bash -c "$(echo -e "\r\n" | curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" 2>&1 
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 # Set environment
-echo 'eval $(~/.linuxbrew/bin/brew shellenv)' >> .bash_profile
-echo 'eval $(~/.linuxbrew/bin/brew shellenv)' >> .profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv) >> .bash_profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv) >> .profile
 
 brew install gcc
