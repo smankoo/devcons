4896d57f6b9a13381951a523f038037f3b1d318e
Sumeet Singh mankoo <sumeet.mankoo@gmail.com>
2020-07-20 10:38:15 -0400

    updated to new config

--- script-library/homebrew.sh
+++ script-library/homebrew.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 # install.sh
 
-URL_BREW='https://raw.githubusercontent.com/Homebrew/install/master/install'
-
-echo -n '- Installing ruby (prerequisite for brew) ... '
-sudo yum -y install ruby
 echo -n '- Installing brew ... '
-echo | /usr/bin/ruby -e "$(curl -fsSL $URL_BREW)"
+echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 #  > /dev/null
 if [ $? -eq 0 ]; then echo 'OK'; else echo 'NG'; fi
